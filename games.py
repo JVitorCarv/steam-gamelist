c0d5e32a94605d075c00b1fe1e4a32ca102ad980
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-04-20 12:41:53 -0300

    Stores every name of each game in list

--- games.py
+++ games.py
@@ -3,6 +3,10 @@ import json
 with open('steamgames.json', 'r', encoding="utf8") as json_file:
     json_load = json.load(json_file)
 
+gamelist = []
 data = json_load['response']['games']
 for game in data:
-    print(game['name'])
+    gamelist.append(game['name'])
+
+for name in gamelist:
+    print(name)
